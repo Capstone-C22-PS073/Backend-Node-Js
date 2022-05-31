d17b744be171c1ef40a5a8fc95207358b35c3b06
Belajar Git <belajargit@student.uns.ac.id>
2022-05-31 23:26:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-
 const __dirname = path.dirname(__filename);
 
 dotenv.config();
